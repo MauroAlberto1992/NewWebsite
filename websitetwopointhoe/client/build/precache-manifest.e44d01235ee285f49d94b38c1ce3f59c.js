6afb9d817416a779cfda2ab6aa6e80a588c5282f
Mauro Alberto <34242274+MauroAlberto1992@users.noreply.github.com>
2020-11-02 23:29:12 +0000

    Repository added

---
+++ websitetwopointhoe/client/build/precache-manifest.e44d01235ee285f49d94b38c1ce3f59c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9672aefef9cb4f4a3395ee522264ef1",
+    "url": "/static/media/mauroAlberto.a9672aef.jpeg"
+  },
+  {
+    "revision": "6b432dac679a90d9c57f",
+    "url": "/static/css/main.ff298c68.chunk.css"
+  },
+  {
+    "revision": "b93faba861f3eba8068386afda4d1f47",
+    "url": "/static/media/logo.b93faba8.png"
+  },
+  {
+    "revision": "1bd4455f6e00090d81cc",
+    "url": "/static/js/1.1bd4455f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b3d98d2c6d726d2ec617ea74b25dc077",
+    "url": "/static/media/CV.b3d98d2c.png"
+  },
+  {
+    "revision": "6b432dac679a90d9c57f",
+    "url": "/static/js/main.6b432dac.chunk.js"
+  },
+  {
+    "revision": "be07f5a2fe53767f9a78d39fb2c90bff",
+    "url": "/static/media/Roll-upV6_sm.be07f5a2.png"
+  },
+  {
+    "revision": "3feaa506196bdbf88f90c072acb5363f",
+    "url": "/static/media/logoEvodeck.3feaa506.png"
+  },
+  {
+    "revision": "89e5dc20144a2be734e21e8e5e76f1cc",
+    "url": "/static/media/logoBlack_semfundo - cópia.89e5dc20.png"
+  },
+  {
+    "revision": "e44218e112163650dfae3ba618e0014d",
+    "url": "/static/media/logoalfr.e44218e1.png"
+  },
+  {
+    "revision": "1bd4455f6e00090d81cc",
+    "url": "/static/css/1.c8ae1ba5.chunk.css"
+  },
+  {
+    "revision": "e23cd752de8f877f599ca84859cf4feb",
+    "url": "/index.html"
+  }
+];
